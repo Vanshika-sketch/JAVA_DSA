eff41e007a1cf601bcedf93041e87b8018e36e0b
vanshika <vanshika11082004@gmail.com>
2026-08-16 22:59:52 +0530

    Solve LeetCode 283: Move Zeroes (two-pointer, in-place)

---
+++ src/LEETCODE/Q283_MovesZeroes.java
@@ -0,0 +1,28 @@
+
+
+package LEETCODE;
+
+import java.util.Arrays;
+
+public class Q283_MovesZeroes {
+    public static void moveZeroes(int[] nums) {
+        int slot = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != 0) {
+                nums[slot] = nums[i];
+                slot++;
+            }
+        }
+        for (int i = slot; i < nums.length; i++) {
+            nums[i] = 0;
+        }
+
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {0, 1, 0, 3, 12};
+        moveZeroes(nums);
+        System.out.println(Arrays.toString(nums));
+    }
+}
