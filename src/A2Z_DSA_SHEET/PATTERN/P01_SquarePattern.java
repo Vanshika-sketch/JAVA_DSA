3d1d17c9657f3561d226b6fd6646c15ce12688b0
vanshika <vanshika11082004@gmail.com>
2026-08-17 12:13:31 +0530

    Implemented using nested for loops (outer=rows, inner=columns)

---
+++ src/A2Z_DSA_SHEET/PATTERN/P01_SquarePattern.java
@@ -0,0 +1,17 @@
+package A2Z_DSA_SHEET.PATTERN;
+
+public class P01_SquarePattern {
+    public static void main(String[] args) {
+        int n = 5;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.print("*");
+
+            }
+            System.out.println();
+
+        }
+
+
+    }
+}
