d414a05bf5ffe556601c8a7489d10bfad758b635
vanshika <vanshika11082004@gmail.com>
2026-08-17 13:07:25 +0530

    P05: Inverted Right Angle Triangle

---
+++ src/A2Z_DSA_SHEET/PATTERN/P05_InvertedRightTriangle.java
@@ -0,0 +1,18 @@
+package A2Z_DSA_SHEET.PATTERN;
+
+public class P05_InvertedRightTriangle {
+    public static void main(String[] args){
+
+        int n=5;
+        for(int i=n;i>=1;i--){
+            for(int j=1;j<=i;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+
+
+
+    }
+}
