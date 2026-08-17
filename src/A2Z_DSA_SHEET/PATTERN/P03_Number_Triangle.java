2c56b1fb4634079661a1a43b3fc8b16b5e5cee4b
vanshika <vanshika11082004@gmail.com>
2026-08-17 12:47:24 +0530

    P03 - Number Triangle

---
+++ src/A2Z_DSA_SHEET/PATTERN/P03_Number_Triangle.java
@@ -0,0 +1,14 @@
+package A2Z_DSA_SHEET.PATTERN;
+
+public class P03_Number_Triangle {
+    public static void main(String[] args) {
+        int n = 5;
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+
+    }
+}
