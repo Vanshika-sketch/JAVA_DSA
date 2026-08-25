38d680064ad125548a6cf705406ecf5eb306188a
vanshika <vanshika11082004@gmail.com>
2026-08-25 13:20:16 +0530

    bubble sort

--- src/A2Z_DSA_SHEET/SORTING/S02_Bubble_sort.java
+++ src/A2Z_DSA_SHEET/SORTING/S02_Bubble_sort.java
@@ -7,13 +7,17 @@ public class S02_Bubble_sort {
         int n=arr.length;
 
         for (int i=0;i<n-1;i++){
-            for(int j=0;j<n-1;j++){
+            boolean swapped =false;
+            for(int j=0;j<n-1-i;j++){
                 if (arr[j]>arr[j+1]){
                     int temp = arr[j+1];
                     arr[j+1]=arr[j];
                     arr[j]= temp;
+                    swapped=true;
+
                 }
             }
+            if(!swapped) break;
         }
 
     }
