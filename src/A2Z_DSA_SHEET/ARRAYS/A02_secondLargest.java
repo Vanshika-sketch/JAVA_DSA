a6ac72313fc2cfaefd7ce6063534789c7348fa08
vanshika <vanshika11082004@gmail.com>
2026-08-29 17:31:00 +0530

    second (largest and smallest)

---
+++ src/A2Z_DSA_SHEET/ARRAYS/A02_secondLargest.java
@@ -0,0 +1,49 @@
+package A2Z_DSA_SHEET.ARRAYS;
+
+public class A02_secondLargest {
+    public static int SecondLargest(int[] arr) {
+
+        int second = Integer.MIN_VALUE;
+        int largest = arr[0];
+        int n = arr.length;
+        for (int i = 1; i < n; i++) {
+            if (arr[i] > largest) {
+                second = largest;
+                largest = arr[i];
+            } else if (arr[i] < largest && arr[i] > second) {
+                second = arr[i];
+            }
+        }
+        return second;
+        }
+        public static int secondSmall(int[] arr){
+        int small = arr[0];
+        int ssmall=Integer.MAX_VALUE;
+        int n= arr.length;
+
+        for(int i =1;i<n;i++){
+            if (arr[i]<small){
+                ssmall=small;
+                small=arr[i];
+            } else if (arr[i]>small && arr[i]<ssmall) {
+                ssmall=arr[i];
+
+            }
+        }
+        return ssmall;
+
+
+
+    }
+    public static void main(String[] args){
+        int[] arr={22,45,65,73,76};
+
+        System.out.println(SecondLargest(arr));
+        System.out.println(secondSmall(arr));
+
+
+
+
+    }
+
+}
