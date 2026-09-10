c369a13edc38e4a9c4429ab4ce9b28da4ebaaabf
vanshika <vanshika11082004@gmail.com>
2026-09-10 11:16:14 +0530

    more optimal solution for rotate array

--- src/LEETCODE/Q189_RotateArray.java
+++ src/LEETCODE/Q189_RotateArray.java
@@ -1,26 +1,33 @@
 package LEETCODE;
 
 public class Q189_RotateArray {
-    public static void rotateArray(int[]arr, int k){
 
-        int n= arr.length;
-        int[] tempArr = new int[n];
-        for(int i=0;i<n;i++){
-            tempArr[(i+k)%n]=arr[i];
+
+    public static void reverse(int[] arr, int start, int end) {
+        while (start < end) {
+            int temp = arr[start];
+            arr[start] = arr[end];
+            arr[end] = temp;
+            start++;
+            end--;
         }
-        for(int i=0;i<n;i++){
-            arr[i]=tempArr[i];
-        }
-
-
-
     }
-    public static void main(String[] args){
-        int arr[] ={1,2,3,4,5,6};
-        rotateArray(arr,3);
-        for(int i=0;i<arr.length;i++) {
+
+
+    public static void rotateArray(int[] arr, int k) {
+        int n = arr.length;
+        k = k % n; // handles k > n case
+
+        reverse(arr, 0, n - 1);
+        reverse(arr, 0, k - 1);
+        reverse(arr, k, n - 1);
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5, 6};
+        rotateArray(arr, 3);
+        for (int i = 0; i < arr.length; i++) {
             System.out.print(arr[i] + " ");
         }
     }
-
 }
